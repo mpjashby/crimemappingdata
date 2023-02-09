448738c21bba2e5f3753289d291fa6f3d39623d4
mpjashby <mpjashby@gmail.com>
2023-02-09 22:47:29 +0000

    Add transcode option to passthrough data gathering

--- data-raw/passthrough.R
+++ data-raw/passthrough.R
@@ -3,8 +3,10 @@
 
 library(tidyverse)
 
+# Download files
 tribble(
   ~url, ~name,
+  "https://services1.arcgis.com/FZVaYraI7sEGQ6rF/arcgis/rest/services/catastro/FeatureServer/10/query?outFields=*&where=1%3D1&f=geojson", "medellin_comunas.geojson",
   "https://opendata.vancouver.ca/explore/dataset/local-area-boundary/download/?format=geojson", "vancouver_neighbourhoods.geojson"
 ) |>
   pwalk(function(url, name) {
@@ -13,3 +15,15 @@ tribble(
       download.file(url = url, destfile = local_file)
     }
   })
+
+
+# Transcode some files to geopackages to save space
+walk(
+  c("medellin_comunas.geojson"),
+  function(name) {
+    old_file <- here::here(str_glue("inst/extdata/{name}"))
+    new_file <- str_replace(old_file, "geojson$", "gpkg")
+    old_file |> read_sf() |> write_sf(new_file)
+    file.remove(old_file)
+  }
+)
